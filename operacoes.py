28390e482391673acc914bc1f30279a628fa49bf
nedymarschuabe <nedymar97@hotmail.com>
2024-05-15 14:34:47 -0300

    criado as funções de cadastrar e listar clientes

--- operacoes.py
+++ operacoes.py
@@ -4,12 +4,14 @@ def menu_principal():
     1 - Sacar
     2 - Depositar
     3 - Extrato
+    4 - Cadastrar Cliente
+    5 - Listar Clientes
     0 - Sair
     """
     return menu
 
 def saque(operacao, saldo, quantidade):
-    print(">>>>>", operacao)
+    print("\n >>>>>", operacao)
     print("OBS.: Pode até 3 saques diários e com limite no máximo de R$500,00 por saque!")
     while True:
         erro = 0
@@ -46,7 +48,7 @@ def saque(operacao, saldo, quantidade):
     return saldo, quantidade
 
 def deposito(operacao, saldo):
-    print(">>>>>", operacao)
+    print("\n >>>>>", operacao)
     while True:
         valor_deposito_string = input("Digite o valor do depósito: ")
         valor_deposito = float(valor_deposito_string.replace(",", "."))
@@ -61,16 +63,67 @@ def deposito(operacao, saldo):
     return saldo
 
 def extrato_conta(operacao, extrato, saldo):
-    print(">>>>>", operacao)
+    print("\n >>>>>", operacao)
     contador = 1
     for transacao in extrato:
         print(f"{contador} - {transacao}")
         contador += 1
     print(f"Saldo: R$ {saldo}")
 
+def cadastro_cliente(operacao, lista_clientes, codigo):
+    print("\n >>>>>", operacao)
+    codigo += 1
+    print(">>> Dados Pessoais")
+    nome = input("Nome completo: ")
+    data_nascimento = input("Data de Nascimento (dd/mm/aaaa): ")
+
+    #validacao CPF
+    cpf_string = input("CPF: ")
+    cpf = ''.join(filter(str.isdigit, cpf_string))
+    for cliente in lista_clientes:
+        if cliente['cpf'] == cpf:
+            print("CPF já cadastrado.")
+
+    print(">>> Dados Endereço")
+    logradouro = input("Logradouro: ")
+    numero = input("Numero: ")
+    bairro = input("Bairro: ")
+    cidade = input("Cidade: ")
+
+    #valida estado
+    while True:
+        estado = input("Siga Estado: ")
+        if len(estado) != 2:
+            print("Formato inválido para a sigla do estado. Por favor, digite um estado válido!")
+            continue
+        else:
+            break
+
+    endereco = logradouro + ", " + numero + " - " + bairro + " - " + cidade + "/" + estado.upper()
+
+    lista_clientes.append({
+        'codigo': codigo,
+        'nome': nome,
+        'data_nascimento': data_nascimento,
+        'cpf': cpf,
+        'endereco': endereco
+    })
+    print("Cliente cadastrado com sucesso!")
+    return codigo
+
+def listar_clientes(operacao, lista_clientes):
+    print("\n >>>>>", operacao)
+    for cliente in lista_clientes:
+        for coluna, valor in cliente.items():
+            print(f"{coluna.capitalize()}: {valor}")
+        print("")
+
 extrato = []
+clientes = []
 saldo_total = 0.0
 quantidade_saque = 1
+status = ""
+codigo_cliente = 0
 
 while True:
     while True:
@@ -86,6 +139,12 @@ while True:
         elif numoperacao == "3":
             descoperacao = 'Extrato'
             extrato_conta(descoperacao, extrato, saldo_total)
+        elif numoperacao == "4":
+            descoperacao = 'Cadastro'
+            codigo_cliente = cadastro_cliente(descoperacao, clientes, codigo_cliente)
+        elif numoperacao == "5":
+            descoperacao = 'Lista de Clientes'
+            listar_clientes(descoperacao, clientes)
         elif numoperacao == "0":
             descoperacao = 'Sair'
             break
